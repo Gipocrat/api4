4b6e4e44aed3fbbeb6c3687ffa33826fe1b0a6a1
Gipocrat <nazarovfeda2000@gmail.com>
2025-03-23 19:46:00 +0300

    Сделал скрипт устойчивым к разрыву соединения

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import telegram
 import random
 from time import sleep
 from dotenv import load_dotenv
+from telegram.error import NetworkError
 
 
 def main():
@@ -16,11 +17,16 @@ def main():
         folder_name= os.environ.get("FOLDER_NAME", "images")
         files=os.listdir(folder_name)
         random.shuffle(files)
-        for file in files:
-            file_path=os.path.join(folder_name, file)
-            with open(file_path, 'rb') as photo:
-                bot.send_photo(chat_id=chat_id, photo=photo)
-            sleep(time_sleep)
+        try:
+            for file in files:
+                file_path=os.path.join(folder_name, file)
+                with open(file_path, 'rb') as photo:
+                    bot.send_photo(chat_id=chat_id, photo=photo)
+                sleep(time_sleep)
+        except NetworkError:
+            print("Ошибка сети. Повторная попытка подключения через 20 секунд")
+            sleep(20)
+            
 
 
 if __name__ == "__main__":
